120686564acf36cfd74efaf76e7d3a50d04b2e03
NicoleLangevin <47360975+NicoleLangevin@users.noreply.github.com>
2019-02-19 18:20:32 -0700

    Update GameConfig.java

--- GameConfig.java
+++ GameConfig.java
@@ -1,3 +1,40 @@
+import java.util.Random;
+import java.util.List;
+
 public class GameConfig{
-	
+    
+    }
+    
+
+    public void gameSetUp(Board emptyBoard){ //emptyBoard is board before we have continents and troops assigned
+	//Call methods to name continents
+	    emptyBoard.setContinents();
+	    emptyBoard.makeListOfCountries();
+	    
+	    int numOfPlayers = emptyBoard.getNumOfPlayers();
+	    emptyPBoard.setNumOfPlayers(numOfPlayers);
+	    int numOfTroopsPerPlayer = emptyBoard.getNumOfTroops(numOfPlayers);
+	    emptyBoard.setupPossession();
+	    emptyBoard.setupTroops();
+        
+    }
+   
+   /** Might need for graphical part
+    public void updateBoard(Board board){
+    }*/
+    
+    public boolean ifWon(){
+        //check if any player has all territories
+	    //if 
+        //check if numOfAdjacent == 0
+    }
+    
+    public boolean validMove(){
+        //continent attacking from is owned by attacker
+        
+    }
+    
+}
+
+
 }
