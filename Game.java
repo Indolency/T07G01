f9ba5eba49ca31d8ac9c38ed4fbc57ee256fe256
NicoleLangevin <47360975+NicoleLangevin@users.noreply.github.com>
2019-02-19 18:15:53 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,5 +1,22 @@
 public class Game{
-
+  
+  private GameConfig config = new GameConfig();
+  
+  public static void main(String[] args){
+    //Make the original gameBoard
+    Board gameBoard = new Board();
+    config.gameSetup(gameBoard);
+    
+    while (config.ifWon == false){
+      humanPlayer1
+        if (config.ifWon == true);
+        break;
+      humanPlayer2
+      //'AIPlayer'
+    }
+    
+  }
+  
   
   
 }
