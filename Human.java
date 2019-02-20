1c078890a735dc677d421115f419a208d5554c81
NicoleLangevin <47360975+NicoleLangevin@users.noreply.github.com>
2019-02-19 18:41:43 -0700

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,3 +1,24 @@
-public class Human{
+import java.util.Scanner;
+import java.util.ArrayList;
 
+public class Human{
+  
+  private Scanner input = new Scanner();
+  private ArrayList<Country> listOfCountries = new ArrayList<Country> ();
+  private ArrayList<Country> listOfPlayerCountries = new ArrayList<Country> ();
+  
+  public void showCountries(Board board){
+    System.out.println("These are the countries you own with the number of troops");
+    listOfCountries = board.getListOfCountries();
+    for (int i=0; i<listOfCountries.size(); i++){
+      if (listOfCountries.get(i).getPossession() == 0){
+        listOfPlayerCountries.add(listOfCountries.get(i));
+      }
+    for (int i=0; i<listOfPlayerCountries; i++){
+      String countryName = listOfPlayerCountries.get(i).getCountryName();
+      int numOfTroops =  listOfPlayerCountries.get(i).getNumOfTroops();
+      System.out.println(countryName+" "+numOfTroops);
+    }
+  }
+  
 }
