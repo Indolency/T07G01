325f880fe880ebe4fdf9a2567d094fcadb970917
NicoleLangevin <47360975+NicoleLangevin@users.noreply.github.com>
2019-02-20 13:58:28 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -2,39 +2,54 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Board{ //extends Board??
+/**
+ * This Board class is part of the Risk game that will initialize the board and update it 
+ * throughout the game. ""NOTE: at this point Board only initializes the board""
+ *
+ * @author Israa Farouk, Jana Osea, Nicole Langevin
+ * @version 1.0
+ * @since 2019-02-19
+ */
 
+public class Board{ 
+
+	/**
+	* Here, @numOfTroopsPP is number of troops per player
+	*/
+	
     private ArrayList<Continent> continents = new ArrayList<Continent>();
     private int numOfPlayers = 0;
     private ArrayList<Country> listOfCountries = new ArrayList<Country>();
-    private int numOfTroops = 0;
+    private int numOfTroopsPP = 0;
+    
+    //creation of all the continents 
+    private Continent NA = new Continent("North America");
+    private Continent SA = new Continent("South America");
+    private Continent EU = new Continent("Europe");
+    private Continent AS = new Continent("Asia");
+    private Continent AF = new Continent("Africa");
+    private Continent AU = new Continent("Australia");
+	
 
 
-	private Continent NA = new Continent("North America");
-	private Continent SA = new Continent("South America");
-	private Continent EU = new Continent("Europe");
-	private Continent AS = new Continent("Asia");
-	private Continent AF = new Continent("Africa");
-	private Continent AU = new Continent("Australia");
-
-    public int getNumOfTroops(int num){//num is num of players
+    public int getNumOfTroopsPP(int num){//num is num of players
         switch(num){
             case 2:
-                numOfTroops = 9;
+                numOfTroopsPP = 9;
             case 3:
-                numOfTroops = 9;
+                numOfTroopsPP = 9;
             case 4:
-                numOfTroops = 9;
+                numOfTroopsPP = 9;
             case 5:
-                numOfTroops = 9;
+                numOfTroopsPP = 9;
             case 6:
-                numOfTroops = 9;
+                numOfTroopsPP = 9;
             case 7:
-                numOfTroops = 9;
+                numOfTroopsPP = 9;
             case 8:
-                numOfTroops = 9;
+                numOfTroopsPP = 9;
         }
-        return numOfTroops;
+        return numOfTroopsPP;
     }
 
     public int getNumOfPlayers(){
@@ -66,7 +81,7 @@ public class Board{ //extends Board??
 
 	public void setupTroops(){
 			int num = getNumOfPlayers();
-			int troops = getNumOfTroops(num);
+			int troops = getNumOfTroopsPP(num);
 			int perPlayer = getNumOfCountries()/getNumOfPlayers();
 			for (int i=0; i < numOfPlayers; i++){// for each player
 				for (int j=0; j < listOfCountries.size(); j++){//for each country
@@ -80,8 +95,6 @@ public class Board{ //extends Board??
 			}
 	}
 
-	public void setNumOfTroopsPerPlayer(){
-	}
 
 	public int getNumOfCountries(){
 		//Need to get remainder for nondivisible countries with more than 2 players
