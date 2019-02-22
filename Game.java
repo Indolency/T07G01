432e0642f779f54bd343269aeb8c9fec384b1999
NicoleLangevin <47360975+NicoleLangevin@users.noreply.github.com>
2019-02-21 19:32:48 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,4 +1,45 @@
 public class Game{
+	
+	public static void main(String[] args){
+		int numOfPlayers = Human.getNumOfPlayers();
+		Board gameBoard = new Board(numOfPlayers);
+		gameBoard.boardSetup();
+		
+		
+		/**
+		while (GameConfig.ifWon() == false){
+			Human.draftTurn();
+			Human.attackTurn();
+			if (GameConfig.ifWon() == true)
+				break;
+			Human.fortifyTurn();
+			AI.draftTurn();
+			AI.attackTurn();
+			if (GameConfig.ifWon() == true)
+				break;
+			AI.fortifyTurn();
+			
+		}
+		*/
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * This is the original class Game for reference
+public class Game{
   
   private GameConfig config = new GameConfig();
   
@@ -16,7 +57,5 @@ public class Game{
     }
     
   }
-  
-  
-  
 }
+*/
