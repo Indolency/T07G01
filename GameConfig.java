8966e440c5f5aea37f4f4e9c65ae9b1e5f6816de
israa-farouk <47348185+israa-farouk@users.noreply.github.com>
2019-02-25 00:23:12 -0700

    Update GameConfig.java

--- GameConfig.java
+++ GameConfig.java
@@ -1,58 +1,49 @@
 import java.util.Random;
-import java.util.List;
+import java.util.ArrayList;
 
 /**
- * The GameConfig Class controls the main loop of the game, which brings
- * together all the classes to run Risk.
- * @author
- * @version 2.0
- * @since 2019-02-23
+ * This GameConfig class is part of the Risk game that is responsible
+ * for the mechanics/rules of the game
+ * @author Israa Farouk
+ * @version 1.0
+ * @since 2019-02-25
  */
+
 public class GameConfig{
-
-
-
-    public void gameSetUp(Board emptyBoard){ //emptyBoard is board before we have continents and troops assigned
-	//Call methods to name continents
-	    emptyBoard.setContinents();
-	    emptyBoard.makeListOfCountries();
-
-	    int numOfPlayers = emptyBoard.getNumOfPlayers();
-	    emptyPBoard.setNumOfPlayers(numOfPlayers);
-	    int numOfTroopsPerPlayer = emptyBoard.getNumOfTroops(numOfPlayers);
-	    emptyBoard.setupPossession();
-	    emptyBoard.setupTroops();
-
-    }
-
-   /** Might need for graphical part
-    public void updateBoard(Board board){
-    }*/
-
-    public boolean ifWon(){
-        //check if any player has all territories
-	    //if
-        //check if numOfAdjacent == 0
-    }
-
-    public boolean validMove(){
-        //continent attacking from is owned by attacker
-
-    }
-
-    public static void main(String[] args) {
-        // set up board (troops, turns, any set up before game starts)
-
-        // Main loop that controls the whole game
-        // while the game is not won
-        // player takes turn
-            // draft
-            // loop attack until player doest not want to attack anymore
-                // or until there are no more places to attack from
-            // fortify
-        // check if won
-        // switch the current player (keep switching back and forth)
-
-    }
-
+	
+     /**
+     * This method analyses the board and checks if the game has been won.
+     * @return won is true if the game has been won and is false otherwise.
+     */
+	
+	public static boolean ifWon(){
+		boolean won = false; 
+		for (int i=0; i < Board.getNumOfPlayers(); i++){// for each player
+			int ccounter = 0; //country counter
+			for (int j=0; j < Board.getListOfCountries().size(); j++){//for each country
+				if (Board.getListOfCountries().get(j).getPossession() == i)//if all the countries are owned by the same player then the game is won
+					ccounter += 1;
+			}
+			if (ccounter == Board.getListOfCountries().size())
+				won = true;
+				break;
+		}
+		return won;
+	}	
+	
+     /**
+     * This method determines how many troops each player receives to draft
+     * based on how many countries they own.
+     * @return pcountries.size() is the number of troops gained by that player 
+     */
+	
+	public static int getNumOfTroopsGained(){ //later will implent continent rewards
+		int pnum = Turn.getPlayerNum(); //gets the current player
+		ArrayList<Country> pcountries = Board.getPlayerCountries(pnum);//gets the list of countries that player owns
+		return pcountries.size(); //returns the number of countries that the player owns which is the size of the list.
+	}
+		
+		
+		
+	
 }
