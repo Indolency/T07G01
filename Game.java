3ca943d96e87f788f1fab7f34da14e040deeb655
israa-farouk <47348185+israa-farouk@users.noreply.github.com>
2019-02-25 00:29:16 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,61 +1,40 @@
+/**
+ * This Game class is the main class of the Risk game
+ * it contains the game loop that runs the game.
+ * @author Israa Farouk
+ * @version 1.0
+ * @since 2019-02-25
+ */
+
+
 public class Game{
 	
 	public static void main(String[] args){
-		int numOfPlayers = Human.getNumOfPlayers();
+		
+		int numOfPlayers = Human.getNumOfPlayers(); //gets the number of players playing the game from the user
 		Board gameBoard = new Board(numOfPlayers);
-		gameBoard.boardSetup();
+		gameBoard.boardSetup(); //board set up
+		gameBoard.boardStatus();//board status displays the ownership and troops in each country
 		
 		
-		/**
-		while (GameConfig.ifWon() == false){
-			Human.draftTurn();
-			Human.attackTurn();
-			if (GameConfig.ifWon() == true)
+		while (GameConfig.ifWon() == false){ //while the game is not won
+			Human.draftTurn(); //player gets to draft
+			Human.attackTurn(); //player gets to attack
+			if (GameConfig.ifWon() == true) //checks if the player's attack was the winning move
 				break;
-			Human.fortifyTurn();
-			AI.draftTurn();
-			AI.attackTurn();
-			if (GameConfig.ifWon() == true)
+			Human.fortifyTurn(); //player gets to fortify
+			AI.draftTurn(); //AI drafts 
+			AI.attackTurn(); //AI gets to attack 
+			if (GameConfig.ifWon() == true) //checks if the AI's attack was the winning move
 				break;
-			AI.fortifyTurn();
+			AI.fortifyTurn(); //AI gets to fortify
 			
 		}
-		*/
+		
+		if (GameConfig.ifWon())
+			System.out.print("the game is won");
 	}
 	
 	
 	
 }
-
-
-
-
-
-
-
-
-
-
-
-/**
- * This is the original class Game for reference
-public class Game{
-  
-  private GameConfig config = new GameConfig();
-  
-  public static void main(String[] args){
-    //Make the original gameBoard
-    Board gameBoard = new Board();
-    config.gameSetup(gameBoard);
-    
-    while (config.ifWon == false){
-      humanPlayer1
-        if (config.ifWon == true);
-        break;
-      humanPlayer2
-      //'AIPlayer'
-    }
-    
-  }
-}
-*/
