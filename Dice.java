80ba985a1f9a92dd32ec9fc68723ce35f0e5f885
NicoleLangevin <47360975+NicoleLangevin@users.noreply.github.com>
2019-02-25 17:19:13 -0700

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,5 +1,14 @@
 import java.util.Random;
 
+/**
+ * This Dice class outputs a random number out of six for each
+ * die and will eventually keep track of all five die.
+ *
+ * @author Nicole Langevin
+ * @version 1.0
+ * @since 2019-02-21
+ */
+
 public class Dice{
 	
 	public static int diceRoll(){
