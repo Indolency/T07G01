8da721a20db347766b6ccc7e942a2f35fc9ec514
israa-farouk <47348185+israa-farouk@users.noreply.github.com>
2019-02-25 17:39:19 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -8,34 +8,39 @@
 
 
 public class Game{
-	
-	public static void main(String[] args){
-		
+
+	public static void setup(){
 		int numOfPlayers = Human.getNumOfPlayers(); //gets the number of players playing the game from the user
 		Board gameBoard = new Board(numOfPlayers);
 		gameBoard.boardSetup(); //board set up
 		gameBoard.boardStatus();//board status displays the ownership and troops in each country
-		
-		
+	}
+
+	public static void play(){
 		while (GameConfig.ifWon() == false){ //while the game is not won
 			Human.draftTurn(); //player gets to draft
 			Human.attackTurn(); //player gets to attack
 			if (GameConfig.ifWon() == true) //checks if the player's attack was the winning move
 				break;
 			Human.fortifyTurn(); //player gets to fortify
-			AI.draftTurn(); //AI drafts 
-			AI.attackTurn(); //AI gets to attack 
+			AI.draftTurn(); //AI drafts
+			AI.attackTurn(); //AI gets to attack
 			if (GameConfig.ifWon() == true) //checks if the AI's attack was the winning move
 				break;
 			AI.fortifyTurn(); //AI gets to fortify
-			
+
 		}
-		
 		if (GameConfig.ifWon()){
 			System.out.println("-----------------------------------");
 			System.out.println("THE GAME HAS BEEN WON!!!!!!!!!!!!!!");
 			System.out.println("-----------------------------------");
 		}
+	}
+
+	public static void main(String[] args){
+
+		setup();
+		play();
 	}
 	
 	
