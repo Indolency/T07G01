03019ac28e8ac24d355419769d7e21538aa45f17
NicoleLangevin <47360975+NicoleLangevin@users.noreply.github.com>
2019-02-25 23:37:10 -0700

    Update Country.java

--- Country.java
+++ Country.java
@@ -8,19 +8,19 @@ public class Country{
  * @version 2.0
  * @since 2019-02-19
  */
+ 
 	/** The variables for number of troops and possession are each assigned to each country
-	* instead of assigning a country to a player
-	*/
-	
-	private String countryName;  
+	 * instead of assigning a country to a player
+	 */
+	private String countryName;
 	private int numOfTroops = 0; //number of troops in each country
 	private int possession = -1; //initial possession value of -1 indicates no ownership
-
-	
+		
+		
 	/** 
-	constructor that takes in one parameter to create a country
-	@param countryName the name of the country 
-	*/
+	 * constructor that takes in one argument 
+	 * @param countryName the name of the country 
+	 */
 	public Country(String countryName){
 		this.countryName = countryName;
 	}
@@ -31,66 +31,74 @@ public class Country{
 		possession = c.getPossession();
 	}
 	
+	public Country(){
+		countryName = "";
+	}
 	
 	// getter methods 
 	
 	/** getter method for the country name
-	* @return countryName returns the name of the country
-	*/
+	 * @return countryName returns the name of the country
+	 */
 	
 	public String getCountryName(){
 		return countryName;
 	}
 	
+	
 	/** getter method for number of troops per country
-	* @return numOfTroops returns the number of troops in that country
-	*/
+	 * @return numOfTroops returns the number of troops in that country
+	 */
 	
 	public int getNumOfTroops(){
 		return numOfTroops;
 	}
 	
+	
 	/** getter method for the ownership of the country
-	* @return posession which is a number that indicates the player's ownership
-	*/
+	 * @return posession which is a number that indicates the player's ownership
+	 */
 	
 	public int getPossession(){
 		return possession;
 	}
 	
+	
 	// setter methods
 	
 	
 	/** setter method for the country name
-	* @param cname the new country name if a change is required 
-	*/
+	 * @param cname the new country name if a change is required 
+	 */
 	
 	public void setCountryName(String cname){
 		countryName = cname;
 	}
+		
 	
 	/** setter method for number of troops in the country
-	* @param tnum the new number of troops the country will contain  
-	*/
+	 * @param tnum the new number of troops the country will contain  
+	 */
 		
-	public void setNumOfTroops(int tnum){
-		numOfTroops = tnum;
+	public void setNumOfTroops(int num){
+		numOfTroops = num;
 	}
-		
+			
 	/** setter method for the ownership of the country
-	* @param poss the new value that indicates the new ownership of the country  
-	*/
+	 * @param poss the new value that indicates the new ownership of the country  
+	 */
 	
 	public void setPossession(int poss){
 		possession = poss;
 	}
+	
 	
 	/**
 	 * @param troops is the number of troops the player is adding to one of their countries
 	 * during the draft part of their turn
 	 */
 	
-	public void addTroops(int troops){
+	public void addNumOfTroops(int troops){
 		numOfTroops += troops;
 	}
 }
